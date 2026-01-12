3fdb0acb84ab6075c48d13dcf7b06c87d68027f9
kuche1 <znamsiparolata4esno@abv.bg>
2026-01-12 19:50:44 +0200

    update

--- config/config.go
+++ config/config.go
@@ -1,7 +1,11 @@
 package config
 
+const NET_REQ_DELAY_MS = 800
+
 // prices are in EUR
 const PRICE_MIN = 1_852 // bgn 3_600
 const PRICE_MAX = 4_632 // bgn 9_000
 
-const NET_REQ_DELAY_MS = 800
+var BLACKLIST_TITLE_PREFIX = []string{
+	"Mercedes",
+}
--- extract_cars/extract_cars.go
+++ extract_cars/extract_cars.go
@@ -2,6 +2,7 @@ package extract_cars
 
 import (
 	"mobilebg2/car"
+	"mobilebg2/config"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -13,18 +14,31 @@ func extractCars(chan_car_pages chan *carPageData, chan_cars chan *car.Car) {
 	for page_data := range chan_car_pages {
 		elem_info := page_data.doc.Find("div.contactsBox").First()
 
-		title := findTitle(elem_info)
+		title, blacklisted := findTitle(elem_info)
+		if blacklisted {
+			continue
+		}
 
 		chan_cars <- car.NewCar(page_data.link, title)
 	}
 }
 
-func findTitle(elem_info *goquery.Selection) string {
+func findTitle(elem_info *goquery.Selection) (value string, blacklisted bool) {
 	elem_title := elem_info.Find("div.obTitle").First()
 
 	title := strings.TrimSpace(elem_title.Text())
 	parts := strings.Split(title, " Обява: ")
 	title = parts[0]
 
-	return title
+	title_lower := strings.ToLower(title)
+
+	for _, blacklisted_title := range config.BLACKLIST_TITLE_PREFIX {
+		blacklisted_title_lower := strings.ToLower(blacklisted_title)
+
+		if strings.HasPrefix(title_lower, blacklisted_title_lower) {
+			return "", true
+		}
+	}
+
+	return title, false
 }
