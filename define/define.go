688277df0982f7731316fb780e2d95e18a6452e1
kuche1 <znamsiparolata4esno@abv.bg>
2026-01-12 19:44:39 +0200

    update

--- define/define.go
+++ define/define.go
@@ -14,3 +14,8 @@ const CAR_LINK_PREFIX = "https:"
 
 // if this is too big, you might miss some of the listings, however if that does happen, a message will be printed
 const PRICE_STEP = 800
+
+const CHAN_BUF_PAGE_WITH_CAR_LINKS = 10
+const CHAN_BUF_CAR_LINKS = 100
+const CHAN_BUF_CAR_PAGES = 100
+const CHAN_BUF_CAR = 100
--- extract_cars/main.go
+++ extract_cars/main.go
@@ -2,17 +2,17 @@ package extract_cars
 
 import (
 	"mobilebg2/car"
-	"mobilebg2/config"
+	"mobilebg2/define"
 	"mobilebg2/net"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func Main(chan_net_req chan *net.ReqData) (chan_cars chan *car.Car) {
-	chan_page_with_car_links := make(chan *goquery.Document, config.CHAN_BUF_PAGE_WITH_CAR_LINKS)
-	chan_car_links := make(chan string, config.CHAN_BUF_CAR_LINKS)
-	chan_car_pages := make(chan *carPageData, config.CHAN_BUF_CAR_PAGES)
-	chan_cars = make(chan *car.Car, config.CHAN_BUF_CAR)
+	chan_page_with_car_links := make(chan *goquery.Document, define.CHAN_BUF_PAGE_WITH_CAR_LINKS)
+	chan_car_links := make(chan string, define.CHAN_BUF_CAR_LINKS)
+	chan_car_pages := make(chan *carPageData, define.CHAN_BUF_CAR_PAGES)
+	chan_cars = make(chan *car.Car, define.CHAN_BUF_CAR)
 
 	go extractSearchPages(chan_net_req, chan_page_with_car_links)
 	go extractCarLinks(chan_page_with_car_links, chan_car_links)
