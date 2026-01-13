e933dba9b1e93709100ede47301a50536d2718f1
kuche1 <znamsiparolata4esno@abv.bg>
2026-01-13 18:41:11 +0200

    update

--- extract_cars/main.go
+++ extract_cars/main.go
@@ -15,10 +15,10 @@ func Main(chan_net_req chan *net.ReqData) (chan_cars chan *car.Car, profiler *Ch
 	chan_cars = make(chan *car.Car, define.CHAN_BUF_CAR)
 
 	channel_profiler := NewChannelProfiler(
-		chan_page_with_car_links,
-		chan_car_links,
-		chan_car_pages,
-		chan_cars,
+		NewChannelData("chan_page_with_car_links", func() int { return len(chan_page_with_car_links) }, cap(chan_page_with_car_links)),
+		NewChannelData("chan_car_links", func() int { return len(chan_car_links) }, cap(chan_car_links)),
+		NewChannelData("chan_car_pages", func() int { return len(chan_car_pages) }, cap(chan_car_pages)),
+		NewChannelData("chan_cars", func() int { return len(chan_cars) }, cap(chan_cars)),
 	)
 
 	go extractSearchPages(chan_net_req, chan_page_with_car_links)
--- extract_cars/profiler.go
+++ extract_cars/profiler.go
@@ -2,58 +2,27 @@ package extract_cars
 
 import (
 	"fmt"
-	"mobilebg2/car"
+	"log"
 	"time"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
+//////////
+////////// channel profiler
+//////////
+
 type ChannelProfiler struct {
-	chan_page_with_car_links chan *goquery.Document
-	chan_car_links           chan string
-	chan_car_pages           chan *carPageData
-	chan_cars                chan *car.Car
-
-	blockcount_running bool
-
-	chan_page_with_car_links_fullcount int
-	chan_car_links_fullcount           int
-	chan_car_pages_fullcount           int
-	chan_cars_fullcount                int
-
-	chan_page_with_car_links_emptycount int
-	chan_car_links_emptycount           int
-	chan_car_pages_emptycount           int
-	chan_cars_emptycount                int
-
-	samples_taken int
+	running      bool
+	samplesTaken int
+	channels     []*ChannelData
 }
 
 func NewChannelProfiler(
-	chan_page_with_car_links chan *goquery.Document,
-	chan_car_links chan string,
-	chan_car_pages chan *carPageData,
-	chan_cars chan *car.Car,
+	channels ...*ChannelData,
 ) *ChannelProfiler {
 	self := &ChannelProfiler{
-		chan_page_with_car_links: chan_page_with_car_links,
-		chan_car_links:           chan_car_links,
-		chan_car_pages:           chan_car_pages,
-		chan_cars:                chan_cars,
-
-		blockcount_running: false,
-
-		chan_page_with_car_links_fullcount: 0,
-		chan_car_links_fullcount:           0,
-		chan_car_pages_fullcount:           0,
-		chan_cars_fullcount:                0,
-
-		chan_page_with_car_links_emptycount: 0,
-		chan_car_links_emptycount:           0,
-		chan_car_pages_emptycount:           0,
-		chan_cars_emptycount:                0,
-
-		samples_taken: 0,
+		running:      false,
+		samplesTaken: 0,
+		channels:     channels,
 	}
 
 	go self.checkBlockings()
@@ -62,74 +31,72 @@ func NewChannelProfiler(
 }
 
 func (self *ChannelProfiler) checkBlockings() {
-	if self.blockcount_running {
-		panic("blockcount_running")
+	if self.running {
+		panic("already started")
 	}
 
-	self.blockcount_running = true
+	self.running = true
 
-	for self.blockcount_running {
-		self.samples_taken += 1
+	for self.running {
+		self.samplesTaken += 1
 
-		// TODO: need to take the lens here in variables
+		for _, channel := range self.channels {
+			length := channel.getLength()
 
-		///// full
+			if length == 0 {
+				channel.countEmpty += 1
+			}
 
-		if len(self.chan_page_with_car_links) == cap(self.chan_page_with_car_links) {
-			self.chan_page_with_car_links_fullcount += 1
+			if length == channel.capacity {
+				channel.capacity += 1
+			}
 		}
 
-		if len(self.chan_car_links) == cap(self.chan_car_links) {
-			self.chan_car_links_fullcount += 1
-		}
-
-		if len(self.chan_car_pages) == cap(self.chan_car_pages) {
-			self.chan_car_pages_fullcount += 1
-		}
-
-		if len(self.chan_cars) == cap(self.chan_cars) {
-			self.chan_cars_fullcount += 1
-		}
-
-		///// empty
-
-		if len(self.chan_page_with_car_links) == 0 {
-			self.chan_page_with_car_links_emptycount += 1
-		}
-
-		if len(self.chan_car_links) == 0 {
-			self.chan_car_links_emptycount += 1
-		}
-
-		if len(self.chan_car_pages) == 0 {
-			self.chan_car_pages_emptycount += 1
-		}
-
-		if len(self.chan_cars) == 0 {
-			self.chan_cars_emptycount += 1
-		}
-
-		/////
-
+		// TODO: this needs to become a constant
 		time.Sleep(time.Millisecond * 100)
 	}
 }
 
 func (self *ChannelProfiler) ShowResults() {
-	self.blockcount_running = false
+	self.running = false
 
 	time.Sleep(time.Millisecond * 100 * 3) // TODO: this is shit
 
-	fmt.Printf("\n")
-	fmt.Printf("Channel Full Counts:\n")
-	fmt.Printf("chan_page_with_car_links: %3v / %3v | %6.2f%%\n", self.chan_page_with_car_links_fullcount, self.samples_taken, 100*float32(self.chan_page_with_car_links_fullcount)/float32(self.samples_taken))
-	fmt.Printf("chan_car_links          : %3v / %3v | %6.2f%%\n", self.chan_car_links_fullcount, self.samples_taken, 100*float32(self.chan_car_links_fullcount)/float32(self.samples_taken))
-	fmt.Printf("chan_car_pages          : %3v / %3v | %6.2f%%\n", self.chan_car_pages_fullcount, self.samples_taken, 100*float32(self.chan_car_pages_fullcount)/float32(self.samples_taken))
-	fmt.Printf("chan_cars               : %3v / %3v | %6.2f%%\n", self.chan_cars_fullcount, self.samples_taken, 100*float32(self.chan_cars_fullcount)/float32(self.samples_taken))
-	fmt.Printf("\n")
-	fmt.Printf("Channel Empty Counts:\n")
-	fmt.Printf("chan_page_with_car_links: %3v / %3v | %6.2f%%\n", self.chan_page_with_car_links_emptycount, self.samples_taken, 100*float32(self.chan_page_with_car_links_emptycount)/float32(self.samples_taken))
-	fmt.Printf("chan_car_links          : %3v / %3v | %6.2f%%\n", self.chan_car_links_emptycount, self.samples_taken, 100*float32(self.chan_car_links_emptycount)/float32(self.samples_taken))
-	fmt.Printf("chan_car_pages          : %3v / %3v | %6.2f%%\n", self.chan_car_pages_emptycount, self.samples_taken, 100*float32(self.chan_car_pages_emptycount)/float32(self.samples_taken))
-	fmt.Printf("chan_cars               : %3v / %3v | %6.2f%%\n", self.chan_cars_emptycount, self.samples_taken, 100*float32(self.chan_cars_emptycount)/float32(self.samples_taken))
+	fmt.Printf("Channels:\n")
+
+	for _, channel := range self.channels {
+		fmt.Printf("    %v:\n", channel.name)
+		fmt.Printf("        Empty: %6.2f%% | %3v / %3v\n", 100*float32(channel.countEmpty)/float32(self.samplesTaken), channel.countEmpty, self.samplesTaken)
+		fmt.Printf("        Full : %6.2f%% | %3v / %3v\n", 100*float32(channel.countFull)/float32(self.samplesTaken), channel.countFull, self.samplesTaken)
+	}
+}
+
+//////////
+////////// channel data
+//////////
+
+type ChannelData struct {
+	name string
+
+	getLength func() int
+	capacity  int
+
+	countEmpty int
+	countFull  int
+}
+
+func NewChannelData(name string, getLength func() int, capacity int) *ChannelData {
+	if capacity <= 0 {
+		log.Printf("Channel profiler will not work correctly with capacity <=0 (in this case %v)", capacity)
+	}
+
+	return &ChannelData{
+		name: name,
+
+		getLength: getLength,
+		capacity:  capacity,
+
+		countEmpty: 0,
+		countFull:  0,
+	}
 }
