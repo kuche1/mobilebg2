2ce1a60f9741429f50474c5cdd09c4c797550f60
kuche1 <znamsiparolata4esno@abv.bg>
2026-01-14 18:58:31 +0200

    update

---
+++ experiment/experiment0/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/bartventer/httpcache"
+	_ "github.com/bartventer/httpcache/store/fscache"
+)
+
+func main() {
+	dsn := "fscache://?appname=mobilebg2" // cache will be located in ~/.cache/mobilebg2
+	client := &http.Client{
+		Transport: httpcache.NewTransport(dsn),
+	}
+
+	resp, err := client.Get("https://example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("body=%s\n", body)
+}
